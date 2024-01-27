e7255c3d9cb72bc15496c2d7bd14540369b79691
aidankeighron <aidankeighron@gmail.com>
2024-01-26 19:24:25 -0500

    longest common sub string

--- Python/test.py
+++ Python/test.py
@@ -1,9 +1,19 @@
-# Variable length parameters
-def languages(**kwargs):
-    for item in kwargs:
-        print(item) # functional # object_oriented
-        print(kwargs[item]) # F# # java
-        
-languages(functional = 'F#', object_oriented = 'java')
-language = {'functional': 'F#', 'object_oriented': 'java'}
-languages(**language) # Same output as previous line
+def longest_common_substring(string1, string2):
+    string1_len = len(string1)
+    string2_len = len(string2)
+
+    dp = [[0] * (string2_len+1) for _ in range(string1_len+1)]
+    ans_index = 0
+    ans_length = 0
+
+    for i in range(1, string1_len+1):
+        for j in range(1, string2_len+1):
+            if string1[i-1] == string2[j-1]:
+                dp[i][j] = 1 + dp[i-1][j-1]
+                if dp[i][j] > ans_length:
+                    ans_index = i
+                    ans_length = dp[i][j]
+    
+    return string1[ans_index - ans_length:ans_index]
+
+print(longest_common_substring("abc", "abc"))
--- docs/assets/Algorithms/Dynamic Programming.md
+++ docs/assets/Algorithms/Dynamic Programming.md
@@ -27,17 +27,34 @@ def longest_common_substring(string1, string2):
     string1_len = len(string1)
     string2_len = len(string2)
 
-    dp = [[0] * (string2_len + 1) for _ in range(string1_len_1)]
+    dp = [[0] * (string2_len+1) for _ in range(string1_len+1)]
     ans_index = 0
     ans_length = 0
 
     for i in range(1, string1_len+1):
         for j in range(1, string2_len+1):
-            if string1[i-1] == string1[j-1]:
+            if string1[i-1] == string2[j-1]:
                 dp[i][j] = 1 + dp[i-1][j-1]
                 if dp[i][j] > ans_length:
                     ans_index = i
                     ans_length = dp[i][j]
     
     return string1[ans_index - ans_length:ans_index]
+```
+
+Explanation:
+
+Using the 2d array `dp` the program keeps track of the number of same characters in a row:
+
+```python
+longest_common_substring("abc", "abc")
+
+dp = [
+    [0, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 2, 0],
+    [0, 0, 0, 3]
+]
+
+output = "abc"
 ```
