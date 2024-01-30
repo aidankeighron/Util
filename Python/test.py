9f9cd6658a35570a48b79c7b5f8afcc9d802d7c0
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2024-01-30 14:03:44 -0500

    permutations

--- Python/test.py
+++ Python/test.py
@@ -1,16 +1,19 @@
-def minimum_cost_path(matrix: list[list]) -> int:
-    # first row
-    for i in range(1, len(matrix[0])):
-        matrix[0][i] += matrix[0][i-1]
+def permutations(collection: list) -> list:
+    res = []
+    collection.sort()
+    cur = []
+    def dfs(i):
+        if cur not in res:
+            res.append(cur.copy())
+        if i >= len(collection):
+            return
+        cur.append(collection[i])
+        dfs(i+1)
 
-    # first column
-    for i in range(1, len(matrix)):
-        matrix[i][0] += matrix[i-1][0]
+        cur.pop()
+        dfs(i+1)
+    dfs(0)
 
-    for i in range(1, len(matrix)):
-        for j in range(1, len(matrix[0])):
-            matrix[i][j] += min(matrix[i-1][j], matrix[i][j-1])
+    return res
 
-    return matrix[-1][-1]
-
-print(minimum_cost_path([[1, 10, 10], [1, 10, 1], [1, 100, 1]]))
+print(permutations([1,2,3,4]))
---
+++ docs/assets/Algorithms/Data Structures.md
@@ -0,0 +1,28 @@
+---
+layout: script
+language: Algorithms
+---
+
+# Permutations
+
+Using a depth first search this algorithm creates a list of all possible reordering of a list. It does this by recursively testing all options.
+
+```python
+def permutations(collection: list) -> list:
+    res = []
+    collection.sort()
+    cur = []
+    def dfs(i):
+        if cur not in res:
+            res.append(cur.copy())
+        if i >= len(collection):
+            return
+        cur.append(collection[i])
+        dfs(i+1) # find all permutations with i
+
+        cur.pop()
+        dfs(i+1) # find all permutations without i
+    dfs(0)
+
+    return res
+```
