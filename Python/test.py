1e5c18441bb742c410c782b780d2791ad597f797
aidankeighron <aidankeighron@gmail.com>
2024-04-12 15:30:03 -0400

    binary tree path sum

--- Python/test.py
+++ Python/test.py
@@ -7,15 +7,42 @@ class Node:
     left: Node | None = None
     right: Node | None = None
 
-def binary_tree_sum(tree: Node) -> int:
-    def dfs(node):
+def binary_tree_path_sum(root: Node, target: int) -> int:
+    res = 0
+
+    def dfs(node, path_sum):
+        nonlocal res
         if node is None:
-            return 0
-        return node.value + dfs(node.left) + dfs(node.right)
-    return dfs(tree)
+            return
+        if path_sum == target:
+            res += 1
+        if node.left:
+            dfs(node.left, path_sum + node.left.value)
+        if node.right:
+            dfs(node.right, path_sum + node.right.value)
 
-tree1 = Node(10)
-tree1.left = Node(5)
-tree1.right = Node(-2)
+    def path_sum(node):
+        if node is None:
+            return
+        dfs(node, node.value)
+        path_sum(node.left)
+        path_sum(node.right)
 
-print(binary_tree_sum(tree1))
+    path_sum(root)
+
+    return res
+
+tree = Node(10)
+tree.left = Node(5)
+tree.right = Node(-3)
+tree.left.left = Node(3)
+tree.left.right = Node(2)
+tree.right.right = Node(11)
+tree.left.left.left = Node(3)
+tree.left.left.right = Node(-2)
+tree.left.right.right = Node(1)
+
+print(binary_tree_path_sum(tree, 8))
+print(binary_tree_path_sum(tree, 7))
+tree.right.right = Node(10)
+print(binary_tree_path_sum(tree, 8))
--- docs/assets/Algorithms/Data Structures.md
+++ docs/assets/Algorithms/Data Structures.md
@@ -123,3 +123,43 @@ def binary_tree_sum(tree: Node) -> int:
         return node.value + dfs(node.left) + dfs(node.right)
     return dfs(tree)
 ```
+
+# Binary Tree Path Sum
+
+Given a root and a target sum find the number of paths that sum up to the target
+
+```python
+from __future__ import annotations
+from dataclasses import dataclass
+
+@dataclass
+class Node:
+    value: int
+    left: Node | None = None
+    right: Node | None = None
+
+def binary_tree_path_sum(root: Node, target: int) -> int:
+    res = 0
+
+    def dfs(node, path_sum):
+        nonlocal res
+        if node is None:
+            return
+        if path_sum == target:
+            res += 1
+        if node.left:
+            dfs(node.left, path_sum + node.left.value)
+        if node.right:
+            dfs(node.right, path_sum + node.right.value)
+
+    def path_sum(node):
+        if node is None:
+            return
+        dfs(node, node.value)
+        path_sum(node.left)
+        path_sum(node.right)
+
+    path_sum(root)
+
+    return res
+```
