5534c7ac2b89d20316ffb07ba21c91165b7377ac
aidankeighron <aidankeighron@gmail.com>
2024-05-17 22:28:38 -0500

    Postfix Evaluation

--- Python/test.py
+++ Python/test.py
@@ -1,15 +1,26 @@
-def next_greatest_element(arr: list) -> list:
+def evaluate(expressions: list[str]) -> float:
     stack = []
-    result = [-1] * len(arr)
-    for i in reversed(range(len(arr))):
-        if stack:
-            while stack[-1] <= arr[i]:
-                stack.pop()
-                if not stack:
-                    break
-        if stack:
-            result[i] = stack[-1]
-        stack.append(arr[i])
-    return result
 
-print(next_greatest_element([-10, -5, 0, 5, 5.1, 11, 13, 21, 3, 4, -21, -10, -5, -1, 0]))
+    operators = {
+        "^": lambda a, b: a**b,
+        "*": lambda a, b: a * b,
+        "/": lambda a, b: a / b,
+        "+": lambda a, b: a + b,
+        "-": lambda a, b: a - b,
+    }
+    for expression in expressions:
+        if expression not in operators:
+            stack.append(float(expression))
+            continue
+        if expression in ["-","+"] and len(stack) < 2:
+            x = stack.pop()
+            if expression == "-":
+                x *= -1
+            stack.append(float(x))
+            continue
+        b = stack.pop()
+        a = stack.pop()
+        stack.append(operators[expression](a,b))
+    return float(stack[0])
+
+print(evaluate(["2", "-1.9", "+", "3", "*"]))
--- docs/assets/Algorithms/Data Structures.md
+++ docs/assets/Algorithms/Data Structures.md
@@ -682,3 +682,34 @@ def next_greatest_element(arr: list) -> list:
         stack.append(arr[i])
     return result
 ```
+
+## Postfix Evaluation
+
+Evaluates a postfix equation.
+
+```python
+def evaluate(expressions: list[str]) -> float:
+    stack = []
+
+    operators = {
+        "^": lambda a, b: a**b,
+        "*": lambda a, b: a * b,
+        "/": lambda a, b: a / b,
+        "+": lambda a, b: a + b,
+        "-": lambda a, b: a - b,
+    }
+    for expression in expressions:
+        if expression not in operators:
+            stack.append(float(expression))
+            continue
+        if expression in ["-","+"] and len(stack) < 2:
+            x = stack.pop()
+            if expression == "-":
+                x *= -1
+            stack.append(float(x))
+            continue
+        b = stack.pop()
+        a = stack.pop()
+        stack.append(operators[expression](a,b))
+    return float(stack[0])
+```
