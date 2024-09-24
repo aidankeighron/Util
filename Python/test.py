c509806b092efd4e94abc136936ac54bd327de8b
aidankeighron <aidankeighron@gmail.com>
2024-09-23 21:51:24 -0400

    Largest Divisible Subset

--- Python/test.py
+++ Python/test.py
@@ -1,9 +1,31 @@
-def iterating_through_submasks(mask: int) -> list:
-    all_submasks = []
-    submask = mask
-    while submask:
-        all_submasks.append(submask)
-        submask = (submask - 1) & mask
+def largest_divisible_subset(array: list) -> list:
+    array.sort()
 
-    return all_submasks
-print(iterating_through_submasks(15))
+    memo = [1] * len(array)
+    hash_array = list(range(len(array)))
+
+    for i, item in enumerate(array):
+        for prev in range(i):
+            if array[prev] != 0 and item % array[prev] == 0 and 1 + memo[prev] > memo[i]:
+                memo[i] = 1 + memo[prev]
+                hash_array[i] = prev
+
+    largest = -1
+    largest_index = -1
+
+    for i, item in enumerate(memo):
+        if item > largest:
+            largest = item
+            largest_index = i
+    
+    if largest_index == -1:
+        return []
+    result = [array[largest_index]]
+    while hash_array[largest_index] != largest_index:
+        largest_index = hash_array[largest_index]
+        result.append(array[largest_index])
+
+    return result
+
+
+print(largest_divisible_subset([1, 16, 7, 8, 4]))
--- docs/assets/Algorithms/Dynamic Programming.md
+++ docs/assets/Algorithms/Dynamic Programming.md
@@ -261,3 +261,38 @@ def iterating_through_submasks(mask: int) -> list:
 
     return all_submasks
 ```
+
+## Largest Divisible Subset
+
+Algorithm to find the biggest subset in the array so any a and d divide into each other
+
+```python
+def largest_divisible_subset(array: list) -> list:
+    array.sort()
+
+    memo = [1] * len(array)
+    hash_array = list(range(len(array)))
+
+    for i, item in enumerate(array):
+        for prev in range(i):
+            if array[prev] != 0 and item % array[prev] == 0 and 1 + memo[prev] > memo[i]:
+                memo[i] = 1 + memo[prev]
+                hash_array[i] = prev
+
+    largest = -1
+    largest_index = -1
+
+    for i, item in enumerate(memo):
+        if item > largest:
+            largest = item
+            largest_index = i
+    
+    if largest_index == -1:
+        return []
+    result = [array[largest_index]]
+    while hash_array[largest_index] != largest_index:
+        largest_index = hash_array[largest_index]
+        result.append(array[largest_index])
+
+    return result
+```
